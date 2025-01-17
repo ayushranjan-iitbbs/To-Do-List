6c6116ba7a8688cc1d9abab02129e34abca62e9f
ayushranjan-iitbbs <codingwithayush10@gmail.com>
2025-01-17 22:33:29 +0530

    Add files via upload

---
+++ todolist.js
@@ -0,0 +1,38 @@
+const taskBox = document.getElementById("task-box");
+const listContainer = document.getElementById("task");
+function addTask() {
+  if (taskBox.value === "") {
+    alert("First Assign yourself some task");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = taskBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  taskBox.value = "";
+  saveData();
+}
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
